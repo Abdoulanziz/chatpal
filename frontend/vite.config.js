3cbc1dba837e43239b7ee49938a922ad9be04a28
Linuxgeekforgeeks <mahmoudalisaaa@gmail.com>
2025-02-08 21:10:11 -0800

    Fixing build yarn

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,10 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  server: { port: "3000" },
+  server: {
+    port: 3000,
+  },
   build: {
-    outDir: "public" // Change output directory to "public"
-  }
-})
+    outDir: 'public',
+    target: 'esnext', // Ensure ESNext target for compatibility
+    commonjsOptions: {
+      transformMixedEsModules: true, // Allow mixed module formats
+    },
+  },
+});
