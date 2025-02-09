910b85f0bb8a52d06de01ab2bc09b589cb7407c1
Abdoulanziz Ally <abdoulanzizally@outlook.com>
2025-02-09 17:59:52 +0300

    update

--- backend/src/index.js
+++ backend/src/index.js
@@ -18,7 +18,7 @@ app.use(express.json({ limit: "10mb" })); // Set JSON payload limit
 app.use(express.urlencoded({ limit: "10mb", extended: true }));
 app.use(
   cors({
-    origin: "http://localhost:3000",
+    origin: "https://chatpal-w16v.onrender.com",
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     credentials: true,
   })
